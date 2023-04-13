a27441dc749463b209bb2271b728de3a52091b19
rafa moral <rafamp89@gmail.com>
2023-04-13 15:50:56 +0200

    feat: add Metrics types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -9,3 +9,52 @@ interface Build {
   branch?: string
   commitHash?: string
 }
+
+type LHVersion = {
+  value: number
+  major: number
+}
+type ClsMetric = {
+  numericUnit: 'millisecond' | null
+  value: number
+}
+type FcpMetric = {
+  numericUnit: 'millisecond' | null
+  value: number
+}
+type FidMetric = {
+  numericUnit: 'millisecond' | null
+  value: number
+}
+type LcpMetric = {
+  numericUnit: 'millisecond' | null
+  value: number
+}
+type SiMetric = {
+  numericUnit: 'millisecond' | null
+  value: number
+}
+type TbtMetric = {
+  numericUnit: 'millisecond' | null
+  value: number
+}
+type TtfbMetric = {
+  numericUnit: 'millisecond' | null
+  value: number
+}
+type TtiMetric = {
+  numericUnit: 'millisecond' | null
+  value: number
+}
+
+interface Metrics {
+  version: LHVersion
+  cls: ClsMetric
+  fcp: FcpMetric
+  fid: FidMetric
+  lcp: LcpMetric
+  si: SiMetric
+  tbt: TbtMetric
+  ttfb: TtfbMetric
+  tti: TtiMetric
+}
