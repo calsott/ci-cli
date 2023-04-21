618cb215056ba646fb69dddf055f24d3aa463b4b
rafa moral <rafamp89@gmail.com>
2023-04-21 15:49:25 +0200

    fix: log pull request data

--- src/adapters/getGithubActionsData.ts
+++ src/adapters/getGithubActionsData.ts
@@ -3,7 +3,6 @@ import {readFile} from 'fs/promises'
 const getGitHubEvent = async () => {
   try {
     const file = await readFile(process.env.GITHUB_EVENT_PATH, 'utf-8')
-    console.log(file)
     return JSON.parse(file)
   } catch (e) {
     return {}
@@ -13,17 +12,24 @@ const getGitHubEvent = async () => {
 export async function getGithubActionsData(): Promise<Build> {
   const githubEvent = await getGitHubEvent()
 
-  console.log(githubEvent.pull_request.number)
-
-  console.log(`>>> ${process.env.GITHUB_REF}`)
   const author =
     githubEvent?.head_commit?.author?.email || process.env.GITHUB_ACTOR
   const branch = process.env.GITHUB_REF_NAME
-  const commitHash = githubEvent?.head_commit?.id
+  const commitHash =
+    githubEvent?.pull_request?.head?.sha || githubEvent?.head_commit?.id
+  const pullRequestId = githubEvent?.pull_request?.number
+
+  console.log({
+    author,
+    branch,
+    commitHash,
+    ...(pullRequestId && {pullRequestId})
+  })
 
   return {
     author,
     branch,
-    commitHash
+    commitHash,
+    ...(pullRequestId && {pullRequestId})
   }
 }
