a745e8c39799f8d7a7563b1719f5798607f7cfe6
rafa moral <rafamp89@gmail.com>
2023-04-24 10:26:07 +0200

    docs: add pullRequestId tag

--- src/providers/datadog/getTagsFromBuild.ts
+++ src/providers/datadog/getTagsFromBuild.ts
@@ -5,5 +5,7 @@ export function getTagsFromBuild(build: Build) {
   const commitTag = commitHash && `commit:${commitHash}`
   const branchTag = branch && `branch:${branch}`
 
+  // Enable pullRequestId tag only if it's a pull request
+
   return [branchTag, commitTag].filter(Boolean)
 }
